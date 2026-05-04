cfa60afafb7f153206fbf6c731ed1e7b04a2d904
tuan45734 <tuanhvph45734@fpt.edu.vn>
2026-05-04 13:57:59 +0700

    t

--- js/login.js
+++ js/login.js
@@ -8,7 +8,7 @@ const USERS = {
     'KV4YXY': { role: 'KV4', name: 'Quản lý KV4' },
     'KV5XXZ': { role: 'KV5', name: 'Quản lý KV5' },
     'KV6XBC': { role: 'KV6', name: 'Quản lý KV6' },
-    'ADMIN99': { role: 'ADMIN', name: 'Quản trị viên' }
+    'ADMIN99': { role: 'ancungbatuyet99', name: 'Quản trị viên' }
 };
 
 // Key lưu trữ session
